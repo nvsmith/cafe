b1098c7c83ee7f4a867c1261a30e5e9347d14af7
Nathan Smith <nathanielvs@gmail.com>
2024-12-04 09:26:28 -0700

    Add component props to menu's Flatlist. Style menu components.

--- app/menu.jsx
+++ app/menu.jsx
@@ -13,19 +13,31 @@ export default function MenuScreen() {
 
     const Container = Platform.OS === "web" ? ScrollView : SafeAreaView;
 
+    const separatorComponent = <View style={styles.separator} />;
+
+    // const headerComponent = <Text>Top of List</Text>;
+    const footerComponent = <Text style={{ color: theme.text }}>End of Menu</Text>;
+
     return (
         <Container>
             <Text>Menu Screen</Text>
             <FlatList
                 data={MENU_ITEMS}
                 keyExtractor={(item) => item.id.toString()}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={styles.contentContainer}
+                ItemSeparatorComponent={separatorComponent}
+                // ListHeaderComponent={headerComponent}
+                ListFooterComponent={footerComponent}
+                ListFooterComponentStyle={styles.footerComponent}
+                ListEmptyComponent={<Text>No Items</Text>}
                 renderItem={({ item }) => (
-                    <View>
-                        <View>
-                            <Text>{item.title}</Text>
-                            <Text>{item.description}</Text>
+                    <View style={styles.row}>
+                        <View style={styles.menuTextRow}>
+                            <Text style={[styles.menuItemTitle, styles.menuItemText]}>{item.title}</Text>
+                            <Text style={styles.menuItemText}>{item.description}</Text>
                         </View>
-                        <Image source={MENU_IMAGES[item.id - 1]} />
+                        <Image source={MENU_IMAGES[item.id - 1]} style={styles.menuImage} />
                     </View>
                 )}
             />
@@ -34,5 +46,55 @@ export default function MenuScreen() {
 }
 
 function createStyles(theme, colorScheme) {
-    return StyleSheet.create({});
+    return StyleSheet.create({
+        contentContainer: {
+            paddingTop: 10,
+            paddingBottom: 20,
+            paddingHorizontal: 12,
+            backgroundColor: theme.background,
+        },
+        separator: {
+            height: 1,
+            backgroundColor: colorScheme === "dark" ? "papayawhip" : "#000",
+            width: "50%",
+            maxWidth: 300,
+            marginHorizontal: "auto",
+            marginBottom: 10,
+        },
+        footerComponent: {
+            color: theme.text,
+            marginHorizontal: "auto",
+        },
+        row: {
+            flexDirection: "row",
+            width: "100%",
+            maxWidth: 600,
+            height: 100,
+            marginBottom: 10,
+            borderStyle: "solid",
+            borderColor: colorScheme === "dark" ? "papayawhip" : "#000",
+            borderWidth: 1,
+            borderRadius: 20,
+            overflow: "hidden",
+            marginHorizontal: "auto",
+        },
+        menuTextRow: {
+            width: "65%",
+            paddingTop: 10,
+            paddingLeft: 10,
+            paddingRight: 5,
+            flexGrow: 1,
+        },
+        menuItemTitle: {
+            fontSize: 18,
+            textDecorationLine: "underline",
+        },
+        menuItemText: {
+            color: theme.text,
+        },
+        menuImage: {
+            width: 100,
+            height: 100,
+        },
+    });
 }
